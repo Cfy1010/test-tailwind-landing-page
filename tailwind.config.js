e4f903504de61c7422343dd48239fd1890caff95
Cfy1010 <cy_one2000@yahoo.fr>
2021-09-12 20:27:56 +0200

    navbar mobile first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    theme: {}
+    theme: {
+        extend: {
+            minHeight: {
+                500: "500px",
+                650: "650px"
+            }
+        }
+    }
 }
